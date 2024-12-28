bd3efbbbfd3e6b63f73211456ae55a77b47592bf
Tousart <150588467+Tousart@users.noreply.github.com>
2024-12-29 00:11:58 +0300

    Add files via upload

---
+++ LC2471_MinimumNumberOperationsSortBT/Solution.java
@@ -0,0 +1,58 @@
+package LC2471_MinimumNumberOperationsSortBT;
+
+import java.util.LinkedList;
+
+public class Solution {
+    int count = 0;
+
+    public int minimumOperations(TreeNode root) {
+        if (root == null) return 0;
+
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int level = queue.size();
+            Integer[] levelList = new Integer[queue.size()];
+
+            for (int i = 0; i < level; i++) {
+                TreeNode current = queue.removeFirst();
+                levelList[i] = current.val;
+
+                if(current.left != null) {
+                    queue.add(current.left);
+                }
+
+                if (current.right != null) {
+                    queue.add(current.right);
+                }
+            }
+
+            sort(levelList);
+        }
+
+        return count;
+    }
+
+    public void sort(Integer[] levelList) {
+        int i = 0;
+
+        while (i < levelList.length - 1) {
+            int j = i + 1;
+            int minIndex = i;
+            while (j < levelList.length) {
+                if (levelList[minIndex] > levelList[j]) {
+                    minIndex = j;
+                }
+                j++;
+            }
+            int temp = levelList[i];
+            levelList[i] = levelList[minIndex];
+            levelList[minIndex] = temp;
+
+            if (i != minIndex) count += 1;
+
+            i++;
+        }
+    }
+}
