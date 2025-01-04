dd8b14ed58c99f7b8e25501e885377bf232c5661
Tousart <150588467+Tousart@users.noreply.github.com>
2025-01-04 11:19:57 +0300

    Add files via upload

---
+++ LC6_ZigzagConversion/Test.java
@@ -0,0 +1,8 @@
+package LC6_ZigzagConversion;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution sl = new Solution();
+        System.out.println(sl.convert("PAYPALISHIRING", 4));
+    }
+}
