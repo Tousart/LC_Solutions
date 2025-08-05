7b98365f8d3a2a65ae15f12908182633e0f826f5
Vsevolod <150588467+Tousart@users.noreply.github.com>
2025-08-06 00:29:54 +0300

    Add files via upload

---
+++ golang_88_lc/solution.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	for n > 0 {
+		if m > 0 && nums1[m-1] > nums2[n-1] {
+			nums1[m+n-1] = nums1[m-1]
+			m--
+		} else {
+			nums1[m+n-1] = nums2[n-1]
+			n--
+		}
+	}
+}
+
+func main() {
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	nums2 := []int{2, 5, 6}
+	merge(nums1, 3, nums2, 3)
+	fmt.Println(nums1)
+}
