ac798abbb4a42e79734377c4f43830ae0f8088d3
Vsevolod <150588467+Tousart@users.noreply.github.com>
2025-08-14 12:16:13 +0300

    Add files via upload

---
+++ golang_206_lc/solution.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+	for head != nil {
+		next := head.Next
+		head.Next = prev
+		prev = head
+		head = next
+	}
+	return prev
+}
+
+func main() {
+	head := ListNode{Val: 1}
+	head.Next = &ListNode{Val: 2}
+	head.Next.Next = &ListNode{Val: 3}
+
+	next := reverseList(&head)
+	for next != nil {
+		fmt.Println(next.Val)
+		next = next.Next
+	}
+}
