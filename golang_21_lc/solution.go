b2a2e77a0644090b76d4c6cf8bcb363b85ae8d49
Vsevolod <150588467+Tousart@users.noreply.github.com>
2025-08-17 12:40:46 +0300

    Add files via upload

--- golang_21_lc/solution.go
+++ golang_21_lc/solution.go
@@ -13,39 +13,38 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 
 	for list1 != nil && list2 != nil {
 		if list1.Val < list2.Val {
-			next.Next = &ListNode{Val: list1.Val}
+			next.Next = list1
 			list1 = list1.Next
 		} else {
-			next.Next = &ListNode{Val: list2.Val}
+			next.Next = list2
 			list2 = list2.Next
 		}
 		next = next.Next
 	}
 
-	for list1 != nil {
-		next.Next = &ListNode{Val: list1.Val}
-		next = next.Next
-		list1 = list1.Next
+	if list1 != nil {
+		next.Next = list1
 	}
 
-	for list2 != nil {
-		next.Next = &ListNode{Val: list2.Val}
-		next = next.Next
-		list2 = list2.Next
+	if list2 != nil {
+		next.Next = list2
 	}
 
 	return result.Next
 }
 
 func main() {
-	list1 := ListNode{Val: 1}
-	list1.Next = &ListNode{Val: 4}
-	list2 := ListNode{Val: 2}
+	l1 := ListNode{Val: 1}
+	l1.Next = &ListNode{Val: 2}
+	l1.Next.Next = &ListNode{Val: 3}
 
-	res := mergeTwoLists(&list1, &list2)
-	next := res
+	l2 := ListNode{Val: 1}
+	l2.Next = &ListNode{Val: 3}
+	l2.Next.Next = &ListNode{Val: 4}
+
+	next := mergeTwoLists(&l1, &l2)
 	for next != nil {
-		fmt.Println(next.Val)
+		fmt.Printf("%d ", next.Val)
 		next = next.Next
 	}
 }
