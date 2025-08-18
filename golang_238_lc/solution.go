0e67007c947a0083f773c7cf2e4784fda27fe1c4
Vsevolod <150588467+Tousart@users.noreply.github.com>
2025-08-18 13:36:25 +0300

    Add files via upload

---
+++ golang_238_lc/solution.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func productExceptSelf(nums []int) []int {
+	res := make([]int, len(nums))
+
+	prefix := 1
+	for i := range len(res) {
+		res[i] = prefix
+		prefix *= nums[i]
+	}
+
+	suffix := 1
+	for i := len(res) - 1; i >= 0; i-- {
+		res[i] *= suffix
+		suffix *= nums[i]
+	}
+
+	return res
+}
+
+func main() {
+	nums := []int{1, 2, 3, 4}
+	fmt.Println(productExceptSelf(nums))
+}
