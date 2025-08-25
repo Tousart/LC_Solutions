bbb83888f39954b212a2f736c4fbdd9d566ec1c9
Vsevolod <150588467+Tousart@users.noreply.github.com>
2025-08-26 01:22:11 +0300

    Add files via upload

--- t_2025_4/solution.go
+++ t_2025_4/solution.go
@@ -7,36 +7,27 @@ import (
 func drain(tanks [][3]int) int {
 	var res int
 	n := len(tanks)
-	trash := make([]int, n)
 	right := make([]int, n)
 	left := make([]int, n)
 
 	// right
-	for i := 1; i < n; i++ {
+	right[n-1] = min(tanks[n-1][2], tanks[n-2][1])
+	for i := n - 2; i > 0; i-- {
 		if tanks[i-1][1] > tanks[i][2] {
-			right[i] = tanks[i][2]
-			trash[i] = tanks[i-1][1] - tanks[i][2]
+			right[i] = min(tanks[i-1][1], tanks[i][2]+right[i+1])
 		} else {
 			right[i] = tanks[i-1][1]
 		}
 	}
 
-	for i := n - 2; i > 0; i-- {
-		right[i] += min(right[i+1], trash[i])
-	}
-
 	// left
-	for i := n - 2; i >= 0; i-- {
+	left[0] = min(tanks[0][2], tanks[1][0])
+	for i := 1; i > n-1; i-- {
 		if tanks[i+1][0] > tanks[i][2] {
-			left[i] = tanks[i][2]
-			trash[i] = tanks[i+1][0] - tanks[i][2]
+			left[i] = min(tanks[i+1][0], tanks[i][2]+left[i-1])
 		} else {
 			left[i] = tanks[i+1][0]
 		}
-	}
-
-	for i := 1; i < n-1; i++ {
-		left[i] += min(left[i-1], trash[i])
 	}
 
 	// answer
@@ -71,3 +62,47 @@ func main() {
 	}
 	fmt.Println(drain(tanks))
 }
+
+// func drain(tanks [][3]int) int {
+// 	var res int
+// 	n := len(tanks)
+// 	trash := make([]int, n)
+// 	right := make([]int, n)
+// 	left := make([]int, n)
+
+// 	// right
+// 	for i := 1; i < n; i++ {
+// 		if tanks[i-1][1] > tanks[i][2] {
+// 			right[i] = tanks[i][2]
+// 			trash[i] = tanks[i-1][1] - tanks[i][2]
+// 		} else {
+// 			right[i] = tanks[i-1][1]
+// 		}
+// 	}
+
+// 	for i := n - 2; i > 0; i-- {
+// 		right[i] += min(right[i+1], trash[i])
+// 	}
+
+// 	// left
+// 	for i := n - 2; i >= 0; i-- {
+// 		if tanks[i+1][0] > tanks[i][2] {
+// 			left[i] = tanks[i][2]
+// 			trash[i] = tanks[i+1][0] - tanks[i][2]
+// 		} else {
+// 			left[i] = tanks[i+1][0]
+// 		}
+// 	}
+
+// 	for i := 1; i < n-1; i++ {
+// 		left[i] += min(left[i-1], trash[i])
+// 	}
+
+// 	// answer
+// 	res = max(tanks[0][2]+right[1], tanks[n-1][2]+left[n-2])
+// 	for i := 1; i < n-1; i++ {
+// 		res = max(res, tanks[i][2]+right[i+1]+left[i-1])
+// 	}
+
+// 	return res
+// }
