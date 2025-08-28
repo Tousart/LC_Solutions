1ecf1d83414af8f37d90b6c90c1a344e2fcb86a7
Vsevolod <150588467+Tousart@users.noreply.github.com>
2025-08-28 15:39:41 +0300

    Add files via upload

---
+++ golang_97_lc/solution.go
@@ -0,0 +1,64 @@
+package main
+
+import "fmt"
+
+func isInterleave(s1 string, s2 string, s3 string) bool {
+	if len(s1)+len(s2) != len(s3) {
+		return false
+	}
+
+	m := len(s1)
+	n := len(s2)
+
+	dp := make([]bool, n+1)
+	dp[0] = true
+	for j := 1; j < n+1; j++ {
+		dp[j] = s2[j-1] == s3[j-1] && dp[j-1]
+	}
+
+	for i := 1; i < m+1; i++ {
+		dp[0] = s1[i-1] == s3[i-1] && dp[0]
+		for j := 1; j < n+1; j++ {
+			dp[j] = (s1[i-1] == s3[i+j-1] && dp[j]) || (s2[j-1] == s3[i+j-1] && dp[j-1])
+		}
+	}
+
+	return dp[n]
+}
+
+func main() {
+	s1 := "aabcc"
+	s2 := "dbbca"
+	s3 := "aadbbcbcac"
+	fmt.Println(isInterleave(s1, s2, s3))
+}
+
+// func isInterleave(s1 string, s2 string, s3 string) bool {
+// 	if len(s1)+len(s2) != len(s3) {
+// 		return false
+// 	}
+
+// 	m := len(s1)
+// 	n := len(s2)
+
+// 	dp := make([][]bool, m+1)
+// 	dp[0] = make([]bool, n+1)
+// 	dp[0][0] = true
+
+// 	for i := 1; i < m+1; i++ {
+// 		dp[i] = make([]bool, n+1)
+// 		dp[i][0] = s1[i-1] == s3[i-1] && dp[i-1][0]
+// 	}
+
+// 	for j := 1; j < n+1; j++ {
+// 		dp[0][j] = s2[j-1] == s3[j-1] && dp[0][j-1]
+// 	}
+
+// 	for i := 1; i < m+1; i++ {
+// 		for j := 1; j < n+1; j++ {
+// 			dp[i][j] = (s1[i-1] == s3[i+j-1] && dp[i-1][j]) || (s2[j-1] == s3[i+j-1] && dp[i][j-1])
+// 		}
+// 	}
+
+// 	return dp[m][n]
+// }
